0fff776182904f19574c318474802a36ff588ad4
NelsonAlmeida-18 <nelson.martinsalmeidaa@gmail.com>
2023-04-01 23:35:59 -0700

    Test

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from fastapi import FastAPI, HTTPException, Query
-from pydantic import BaseModel
-from classes import *
+from flask import Flask, jsonify
+import os
 from backend import *
-import datetime
-import json
 
-app = FastAPI()
+app = Flask(__name__)
 my_backend = backend()
 
-@app.post("/signup")
-async def createAccount(conta:SignUp):
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route("/signup", methods=["POST"])
+def createAccount(conta):
     if conta!=None:
-        statusCode, payload = my_backend.createNewUser({"email":conta.email, "pwd":conta.pwd})
+        statusCode, payload = my_backend.createNewUser({"email":conta["email"], "pwd":conta["pwd"]})
         if statusCode!=200:
-            raise HTTPException(status_code = 422, detail="User already exists")
+            return ""
         return payload
-
-@app.post("/login")
-async def login(user:Login):
-    statusCode, result = my_backend.login({"id":user.id, "pwd": user.pwd})
-
-    if statusCode!=200:
-        raise HTTPException(status_code=422, detail="Invalid username or password")
-    
-    return result
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
